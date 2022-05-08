95b97922258b7bc36e23e7717b5eaa33f35f6f27
jedihilario <hyanezbertola@gmail.com>
2022-05-08 20:52:43 -0300

    Initial Commit

---
+++ clicker.md
@@ -0,0 +1,19 @@
+# Clicker
+
+## Interfaz:
+    - Zona del clicker -> (Galleta, acceso a otras interfaces, mejoras si las compra)
+    - Tienda -> (Mejoras, precio)
+
+## Reglas:
+    - 1 click =  1 Galleta
+    - 1 cursor empieza en 15 galletas
+    - Por cada compra de una mejora, su precio aumenta
+    - 1 cursor hace 1 click cada 10 seg
+    
+## Items
+    - Cursores
+    - Abuelas
+    - Fabricas
+
+## Upgrades
+    - Con bastante monedas, se pueden ir comprando mejoras para poder conseguir mas clicks o potenciar los cursores o mejoras compradas
---
+++ game.js
@@ -0,0 +1,58 @@
+class ShopElement {
+    constructor(name, img, incrementValue, incrementTime, price) {
+        this.name = name;
+        this.img = img;
+        this.incrementValue = incrementValue;
+        this.incrementTime = incrementTime;
+        this.price = price;
+        
+        this.intervals = [];
+    }
+
+    set value(newValue) {
+        this.incrementValue = newValue;
+    }
+
+    set time(newTime) {
+        this.incrementTime = newTime;
+    }
+
+    createInterval(intervalTime, intervalValue) {
+        return setInterval(() => {
+            contador.innerHTML = Number(contador.innerHTML) + intervalValue;
+        }, intervalTime);
+    }
+
+    createElement() {
+        const container = document.createElement('DIV');
+        container.classList.add('shop-element');
+        container.innerHTML = `
+            <img src="${this.img}">
+            <div class="element__info">
+                <h3>${this.name}</h3>
+                <h4 id="element__price">${this.price}</h4>
+            </div>
+        `;
+        container.addEventListener('click', () => {
+            if(Number(contador.innerHTML) >= this.price) {
+                this.intervals.push(this.createInterval(this.incrementTime, this.incrementValue));
+                contador.innerHTML = Number(contador.innerHTML) - this.price;
+                this.price = Math.floor(this.price * 1.25);
+                document.getElementById('element__price').innerHTML = this.price;
+            }
+        });
+
+        return container;
+    }
+}
+
+const clicker = document.getElementById('clicker');
+const contador = document.getElementById('contador');
+const shop = document.querySelector('.shop');
+
+clicker.addEventListener('click', () => {
+    contador.innerHTML = Number(contador.innerHTML) + 1;
+});
+
+const cursor = new ShopElement('Cursor', 'img/fotito.jpg', 1, 2000, 15);
+shop.appendChild(cursor.createElement());
